936ccf3381c1ca717aa509145fbd880c15367e68
Ryan <eckerle_r@protonmail.com>
2018-08-15 03:47:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget(nest, target) {
+  return document.querySelector('#nested div.target')
+}
